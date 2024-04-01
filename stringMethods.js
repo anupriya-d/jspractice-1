0e430815d3bc66a97c15fc448cdd193b45eb256f
Anupriya Dayaratne <anupriya.dayaratne@gmail.com>
2024-04-02 00:25:33 +1300

    Modified

--- stringMethods.js
+++ stringMethods.js
@@ -1,1 +1,20 @@
 //string
+
+//Character access
+
+let greet = "Hello";
+
+//1. charAt to get Letter by index
+
+greet.charAt(0);
+
+console.log(greet.charAt(0));
+
+console.log('Hello'[0]);
+//2. indexof gives indexof first occurence
+
+console.log(greet.indexOf("l"));
+
+//last occurence
+
+console.log(greet.lastIndexOf("l"));
